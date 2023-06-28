ddf795148e827908290bdc6053a269532036b621
osappy <904929+osappy@users.noreply.github.com>
2023-06-28 11:10:48 +0900

    disable server feature

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { AppController } from './app.controller';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.get(AppController).job();
+  app.close();
 }
+
 bootstrap();
